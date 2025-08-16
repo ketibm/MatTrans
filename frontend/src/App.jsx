b24b38f04df3ca6f0e5583d49c3235c9450fe492
ketibm <k.matenicharska@outlook.com>
2025-08-16 13:23:18 +0200

    mob1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import HomePage from "./pages/HomePage/HomePage.jsx";
 import PrivacyTermsPage from "./pages/Privacy/PrivacyTermsPage.jsx";
 import AdminReservationsPanel from "./pages/Admin/AdminReservationsPanel.jsx";
 import ScrollToTop from "./components/Scroll/ScrollToTop.jsx";
-import AdminLoginPage from "./pages/Admin/AdminLoginPage.jsx";
 import RequireAuth from "./components/Auth/RequireAuth.jsx";
 import AdminAuthPage from "./pages/Admin/AdminAuthPage.jsx";
 import ResetPasswordPage from "./pages/Admin/ResetPasswordPage.jsx";
@@ -20,7 +19,7 @@ function App() {
         <Route element={<Root />}>
           <Route path="/" element={<HomePage />} />
           <Route path="/privacy" element={<PrivacyTermsPage />} />
-          <Route path="/privacy" element={<PrivacyTermsPage />} />
+
           <Route path="/login" element={<AdminAuthPage />} />
           <Route path="/reset-password" element={<ResetPasswordPage />} />
           <Route element={<RequireAuth />}>
